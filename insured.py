339af74ef539ea59045d1b5d4fd76062d28ad83b
Jakub Chochola <chochola444@gmail.com>
2022-07-27 11:04:42 +0200

    Comments added, minor find_user method upgrade.

--- insured.py
+++ insured.py
@@ -1,12 +1,18 @@
 class Insured:
 
     def __init__(self, first_name, last_name, phone_number, age):
+        """
+        Creates new insured person, based on the inputed first name, last name, phone number and age.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
         self.age = age
 
     def __str__(self):
+        """
+        Returns first name, last name, phone number and age of the insured person.
+        """
         return "{0} {1} {2} {3}".format(self.first_name, self.last_name, self.phone_number, self.age)
 
 
--- interface.py
+++ interface.py
@@ -2,14 +2,23 @@ class Interface:
 
     @staticmethod
     def list_of_insured(insured_persons):
+        """
+        Returns all insured persons in the list.
+        """
         for person in insured_persons:
             print(person)
 
     @staticmethod
     def find_user(insured_persons):
+        """
+        Finds user based on the searched name and surname.
+        """
         insureds_first_name = input("Enter the insured's first name:\n")
         insureds_last_name = input("Enter the insured's last name:\n")
         print()
         for person in insured_persons:
             if insureds_first_name in person and insureds_last_name in person:
                 print(person.__str__(), "\n")
+            else:
+                print("This person is not in the database.")
+                print()
